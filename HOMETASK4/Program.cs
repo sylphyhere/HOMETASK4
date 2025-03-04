fbd70ca26f2e6b9a35669822228940f7f8ae2861
Administrator <Administrator@NAEEM>
2025-03-05 00:40:37 +0500

    Add project files.

---
+++ HOMETASK4/Program.cs
@@ -0,0 +1,52 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOMETASK4
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public string ISBN;
+        public double Price;
+        public Book() { }
+        void DisplayBookInfo()
+        {
+            Console.WriteLine("Book Info is Displayed here : \n");
+            Console.WriteLine($"Title : {Title} \n");
+            Console.WriteLine($"Author : {Author} \n");
+            Console.WriteLine($"ISBN : {ISBN} \n");
+            Console.WriteLine($"Price : {Price} \n");
+        }
+        void ApplyDiscount(double percentage)
+        {
+            if (percentage < 0 || percentage > 100)
+            {
+                Console.WriteLine("Discount percentage should be between 0 and 100");
+                return;
+            }
+            double discountPrice = Price * (percentage / 100);
+            Price = Price - discountPrice;
+        }
+
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.Title = "It ends with US";
+            book1.Author = "Colleen Hoover";
+            book1.ISBN = "2345";
+            book1.Price = 345;
+            book1.DisplayBookInfo();
+
+            Console.WriteLine("After applying discount : ");
+
+            book1.ApplyDiscount(20);
+            book1.DisplayBookInfo();
+        }
+    }
+}*/
---
+++ HOMETASK4/QUESTION 2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOME_TASK_4
+{
+    internal class Employee
+    {
+        public int EmployeeID;
+        public string Name;
+        public string Department;
+        public double Salary;
+        public Employee(int employeeID,string name,string department,double salary)
+        {
+            EmployeeID = employeeID;
+            Name = name;
+            Department = department;
+            Salary = salary;
+        }
+        void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("Employee details are displayed here : ");
+            Console.WriteLine($"EmployeeID : {EmployeeID}");
+            Console.WriteLine($"Employee Name : {Name}");
+            Console.WriteLine($"Employee's Department : {Department}");
+            Console.WriteLine($"Employee's Salary : {Salary}");
+        }
+        void IncreaseSalary(double amount)
+        {
+            if(amount< 0)
+            {
+                Console.WriteLine("The amount must be positive value");
+                return;
+            }
+            Salary = Salary + amount;
+        }
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(125,"Khadija","Aviation",765890);
+            employee1.EmployeeID = 125;
+            employee1.Name = "Khadija";
+            employee1.Department = "Aviation";
+            employee1.Salary = 765890;
+
+            employee1.DisplayEmployeeDetails();
+            employee1.IncreaseSalary(45667);
+            Console.WriteLine("\n After increasing salary : \n");
+            employee1.DisplayEmployeeDetails();
+
+        }
+    }
+}
